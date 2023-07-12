d83e6e0b20f3adaba00ac64f22b7176199ef5532
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-13 00:10:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("name")
+const password = document.getElementById("password")
+const phone = document.getElementById("phone")
+const email = document.getElementById("email")
+const age = document.getElementById("age")
